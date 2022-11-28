a31e914980e8bc18111745e30ff6dc99ff4da079
kerrrusha <brcyrill2@gmail.com>
2022-11-28 17:45:10 +0200

    signout bug fix

--- src/main/java/com/kerrrusha/playlistassistant/controller/auth/SignoutServlet.java
+++ src/main/java/com/kerrrusha/playlistassistant/controller/auth/SignoutServlet.java
@@ -2,7 +2,6 @@ package com.kerrrusha.playlistassistant.controller.auth;
 
 import com.kerrrusha.playlistassistant.service.auth.AuthService;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,8 +10,8 @@ import java.io.IOException;
 public class SignoutServlet extends HttpServlet {
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		new AuthService(request).signout();
-		request.getRequestDispatcher(request.getContextPath() + "/index.jsp").forward(request, response);
+		response.sendRedirect("../");
 	}
 }
